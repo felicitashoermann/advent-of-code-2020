44402ddf2493bd5dbf8740e9ed3b4a528600a5e5
felicitashoermann <43412653+felicitashoermann@users.noreply.github.com>
2020-12-01 18:55:44 +0100

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    kotlin("jvm") version "1.4.20"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter:5.7.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
